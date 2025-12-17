fb28fa504d77c296106f26f2b194b8e40c027d25
Ushain007 <ushain.mi293@hotmail.com>
2025-12-17 20:30:17 +0600

    go

--- src/main/java/com/example/oop0029/Scene2DataGenController.java
+++ src/main/java/com/example/oop0029/Scene2DataGenController.java
@@ -1,8 +1,44 @@
 package com.example.oop0029;
 
-public class Scene2DataGenController
-{
-    @javafx.fxml.FXML
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
+public class Scene2DataGenController {
+
+    @FXML
+    private TableView<Monitor_2320676> monTableView;
+
+    @FXML
+    private TableColumn<Monitor_2320676, String> nameCol;
+
+    @FXML
+    private TableColumn<Monitor_2320676, String> resCol;
+
+    @FXML
+    private TableColumn<Monitor_2320676, Integer> diaCol;
+
+    @FXML
+    private TableColumn<Monitor_2320676, Integer> ppiCol;
+
+    @FXML
+    private Label statusLabel;
+
+    private ObservableList<Monitor_2320676> monitorList =
+            FXCollections.observableArrayList();
+
+    // ---------- INITIALIZE ----------
+    @FXML
     public void initialize() {
 
         nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
